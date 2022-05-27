ef7e5cc09a641329ff98b669c141a18f7e954ca5
Davide Murroni <davide.murroni@edu.itspiemonte.it>
2022-05-27 12:16:49 +0200

    added data on form.php / send to db working

--- form.php
+++ form.php
@@ -11,15 +11,33 @@ if (isset($_POST['fname'])) {
 
 
 $connection = new mysqli($servername, $username, $password, $dbname);
-$fname = $connection->real_escape_string($_POST['fname']);
-$mname = $connection->real_escape_string($_POST['mname']); 
-$lname = $connection->real_escape_string($_POST['lname']);
-$email = $connection->real_escape_string($_POST['email']);
+$fName = $connection->real_escape_string($_POST['fname']);
+$mName = $connection->real_escape_string($_POST['mname']); 
+$lName = $connection->real_escape_string($_POST['lname']);
 $battleTag = $connection->real_escape_string($_POST['btag']);
+$teamName = $connection->real_escape_string($_POST['tname']);
+$email = $connection->real_escape_string($_POST['email']);
+$telephone = $connection->real_escape_string($_POST['phone']);
+$city = $connection->real_escape_string($_POST['city']);
+$userAddress = $connection->real_escape_string($_POST['address']);
+$country = $connection->real_escape_string($_POST['country']);
+$age = $connection->real_escape_string($_POST['age']);
+$gender = $connection->real_escape_string($_POST['gender']);
+$hair = $connection->real_escape_string($_POST['hair-color']);
+$eye = $connection->real_escape_string($_POST['eye-color']);
+$tournaments = $connection->real_escape_string($_POST['tournaments']);
 
 
-$query = "INSERT INTO  players (firstname, middlename, surname,  mail, battletag)
-        VALUES('$fname', '$mname', '$lname', '$email', '$battleTag')";
+
+
+$query = "INSERT INTO  players
+ (firstName, middleName, surname,
+ battleTag, teamName,  mail,
+ phone, city, userAddress, country, age, gender,
+ hair, eyes, tournaments)
+        VALUES('$fName', '$mName', '$lName', '$battleTag', 
+        '$teamName', '$email', '$telephone', '$city', '$userAddress', 
+        '$country', '$age', '$gender' '$hair', '$eye', '$tournaments')";
     if($connection->query($query) === true ) {
         echo "ok";
         header("Location: ./form.html");
@@ -27,5 +45,3 @@ $query = "INSERT INTO  players (firstname, middlename, surname,  mail, battletag
         echo "no";
     }
 }
-?>
-
