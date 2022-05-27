068a2c989cb854fc2617a8af6327b01eaa0e2961
KelosDev <gpiraswebdev@gmail.com>
2022-05-27 03:35:07 +0200

    aggiunta freccia quando l'header allo scroll si nasconde

--- scripts/news.js
+++ scripts/news.js
@@ -11,16 +11,37 @@ hamburger.addEventListener("click", function () {
 let lastScrollTop = 0;
 
 let menuHidden = document.querySelector(".header");
+let arrowDown = document.querySelector(".arrow-down");
 
 window.addEventListener("scroll", function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
         menuHidden.style.top = "-100px";
+
     } else {
         menuHidden.style.top = "0";
+
     }
+
     lastScrollTop = scrollTop;
 })
 
 
+// fa comparire la freccia quando l'header si nasconde
+
+window.addEventListener("scroll", function () {
+    if (menuHidden.style.top == "-100px") {
+        arrowDown.classList.add("visible");
+
+    } else {
+        arrowDown.classList.remove("visible");
+    }
+})
+
+
+// se il menu è nascosto e lo "clicki" torna giù
+
+window.addEventListener("click", function () {  
+        menuHidden.style.top = "0px";
+})
