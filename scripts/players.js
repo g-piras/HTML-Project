ffb4a1ef285c5a85ced55b378efd02f9e0e854c3
Eloise Bryony Giorda <ICTS21-23.053@studenti.local>
2022-05-28 15:03:41 +0200

    players mobile v2

--- scripts/players.js
+++ scripts/players.js
@@ -1,3 +1,100 @@
+
+//toggle - menu a tendina
+
+
+
+
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".menu");
+hamburger.addEventListener("click", function () {
+    menu.classList.toggle("menu--open");
+});
+
+
+
+//click arrow to scroll down by 508px
+
+let arrow = document.querySelector(".bottom-arrow");
+arrow.addEventListener("click", function () {
+    window.scrollTo(0, 600);
+});
+
+
+
+
+
+
+// Hide menu on scroll
+
+let lastScrollTop = 0;
+
+let menuHidden = document.querySelector(".header");
+let arrowDown = document.querySelector(".arrow-down");
+
+window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        menuHidden.style.top = "-100px";
+        
+    } else {
+        menuHidden.style.top = "0";
+        
+    }
+
+    lastScrollTop = scrollTop;
+})
+
+
+
+// fa comparire la freccia quando l'header si nasconde
+
+window.addEventListener("scroll", function () {
+    if (menuHidden.style.top == "-100px") {
+        arrowDown.classList.add("visible");
+
+    } else {
+        arrowDown.classList.remove("visible");
+    }
+})
+
+
+
+
+// se il menu è nascosto e lo "clicki" torna giù
+
+window.addEventListener("click", function () {
+    menuHidden.style.top = "0px";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let trophyIcon = "https://img.icons8.com/ios-glyphs/344/ffffff/trophy.png";
 
 function generatePlayer(playerInfo, container) {
@@ -130,7 +227,7 @@ readTextFile(
         let targetPosition = playerName.getBoundingClientRect().top;
         let screenPosition;
         
-        screenPosition = window.innerHeight / 1.3;
+        screenPosition = window.innerHeight/1.1;
         if (targetPosition < screenPosition) {
           playerName.className = "player-name";
         } else {
